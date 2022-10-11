10426c2facd449f01b91059c4dc159220fe6864d
Jeevan1016 <112477902+Jeevan1016@users.noreply.github.com>
2022-10-11 15:33:19 +0530

    x=int(input()) for j in range(x):     n=int(input())     a=n     b=n     while True:         s=0         for i in range(1,a+1):             if a%i==0:                 s+=1         if s==2:             break         a-=1     while True:         s=0         for i in range(1,b+1):             if b%i==0:                 s+=1         if s==2:             break         b+=1     if n-b<=a-n:         print(a)     else:         print(b)

---
+++ Nearest_Prime.py
@@ -0,0 +1,25 @@
+x=int(input())
+for j in range(x):
+    n=int(input())
+    a=n
+    b=n
+    while True:
+        s=0
+        for i in range(1,a+1):
+            if a%i==0:
+                s+=1
+        if s==2:
+            break
+        a-=1
+    while True:
+        s=0
+        for i in range(1,b+1):
+            if b%i==0:
+                s+=1
+        if s==2:
+            break
+        b+=1
+    if n-b<=a-n:
+        print(a)
+    else:
+        print(b)
